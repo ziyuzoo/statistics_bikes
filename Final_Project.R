3f0b7988b226fd65dc62fa635dbf5ad50fa6e47b
tiffaniiieee <32556120+tiffaniiieee@users.noreply.github.com>
2018-04-30 14:31:32 -0400

    Started to identify outliers in the dataset

--- Final_Project.R
+++ Final_Project.R
@@ -16,9 +16,16 @@ vifstep(x.bike, th = 10) # no predictor variables are correlated with each other
 # initial model
 initial.bike.fit = lm(cnt ~ mnth + temp + weekday + workingday + 
                         weathersit + holiday + hum + windspeed)
+plot(initial.bike.fit) # possible outliers: Observations 203, 204, 442
 scatter.smooth(resid(initial.bike.fit) ~ predict(initial.bike.fit)) # residual plot has linear pattern
 
-# box cox transformation
-boxcox(initial.bike.fit) # choose lambda = 0.75
+# determine outlying observations
+rstudent(initial.bike.fit)
+qt(0.975, df = 731 - 8 - 1)
 
+# remove possible outliers
+bike.revised = bike.data[-c(203, 204, 442),]
+model2 = lm(cnt ~ mnth + temp + weekday + workingday + 
+              weathersit + holiday + hum + windspeed, data = data.frame(bike.revised))
+scatter.smooth(resid(model2) ~ predict(model2))
 
